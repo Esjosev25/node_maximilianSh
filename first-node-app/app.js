01cdba77623cbfa394166333b0b3e30e93748f31
Estuardo Villeda <estuardovilleda2001@gmail.com>
2022-12-05 18:55:24 -0600

    chore: introduction using express

--- first-node-app/app.js
+++ first-node-app/app.js
@@ -1,9 +1,23 @@
-const http = require('http');
+const express = require('express');
+var fs = require('fs');
+var morgan = require('morgan');
+var path = require('path');
+const app = express();
 
-const routes = require('./routes');
+// create a write stream (in append mode)
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {
+  flags: 'a',
+});
 
-console.log(routes.someText);
-
-const server = http.createServer(routes.handler);
-
-server.listen(3000);
+// setup the logger
+app.use(morgan('combined', { stream: accessLogStream }));
+app.use(morgan('dev'));
+app.use((req, res, next) => {
+  console.log('In the middleware');
+  next();
+});
+app.use((req, res, next) => {
+  console.log('In another middleware');
+  res.send(`<h1>Hola from express!</h1>`);
+});
+app.listen(3000);
