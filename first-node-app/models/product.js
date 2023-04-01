e363316b08201f670251dd2305c32e3ad831533e
Estuardo Villeda <estuardovilleda2001@gmail.com>
2023-04-01 09:14:46 -0600

    feat: storage products in a json

--- first-node-app/models/product.js
+++ first-node-app/models/product.js
@@ -1,13 +1,32 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
+ const p = path.join(process.cwd(), 'data', 'products.json');
+      
+const getProductsFromFile = (cb) => {
+  
+
+  fs.readFile(p, (err, fileContent) => {
+    if (err) 
+      cb([]);
+    else
+    cb(JSON.parse(fileContent));
+  });
+};
 module.exports = class Product {
   constructor(title) {
     this.title = title;
   }
   save() {
-    products.push(this)
+    getProductsFromFile((products) => {
+     
+      products.push(this);
+      fs.writeFile(p,JSON.stringify(products), (err,fileContent)=>{
+      })
+    });
   }
 
-  static fetchAll(){
-    return products;
+  static fetchAll(cb) {
+   getProductsFromFile(cb);
+
   }
 };
