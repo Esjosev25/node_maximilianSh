d8e266dcab8fee75478f542a14cf710e5e12c9da
Estuardo Villeda <estuardovilleda2001@gmail.com>
2023-04-01 23:14:01 -0600

    chore: separar rutas por admin y shop, y agregar vistas

--- first-node-app/routes/admin.js
+++ first-node-app/routes/admin.js
@@ -5,13 +5,16 @@ const express = require('express');
 const rootDir = require('../util/path');
 
 const router = express.Router();
-const productsController = require('../controllers/product');
+const adminController = require('../controllers/admin');
 
 
 // /admin/add-product => GET
-router.get('/add-product', productsController.getAddProduct);
+router.get('/add-product', adminController.getAddProduct);
 
 // /admin/add-product => POST
-router.post('/add-product', productsController.postAddProduct);
+router.post('/add-product', adminController.postAddProduct);
+
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
 
 module.exports = router;
